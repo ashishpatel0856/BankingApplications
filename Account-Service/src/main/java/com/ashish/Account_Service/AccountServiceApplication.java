95a56da3af5120a5a746c3ef15390a531145ff0b
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-08-06 14:55:02 +0530

    changes

--- Account-Service/src/main/java/com/ashish/Account_Service/AccountServiceApplication.java
+++ Account-Service/src/main/java/com/ashish/Account_Service/AccountServiceApplication.java
@@ -3,6 +3,7 @@ package com.ashish.Account_Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AccountServiceApplication {
 
