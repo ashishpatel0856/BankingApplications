c37fcb5dd26da26d5f98d432574b45872a30b9cb
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-08-17 15:24:58 +0530

    changes

---
+++ APIGateway-Service/src/test/java/com/ashish/APIGateway_Service/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.APIGateway_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
