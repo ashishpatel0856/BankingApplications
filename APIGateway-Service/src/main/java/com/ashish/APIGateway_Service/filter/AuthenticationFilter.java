5c8197e61be0d40f0ad51c31c2b90ca92e928182
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-09-06 15:39:52 +0530

    add

--- APIGateway-Service/src/main/java/com/ashish/APIGateway_Service/filter/AuthenticationFilter.java
+++ APIGateway-Service/src/main/java/com/ashish/APIGateway_Service/filter/AuthenticationFilter.java
@@ -1,4 +1,58 @@
 package com.ashish.APIGateway_Service.filter;
 
-public class AuthenticationFilter {
+import com.ashish.APIGateway_Service.JwtService;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+@Slf4j
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final JwtService jwtService;
+
+    public AuthenticationFilter(JwtService jwtService) {
+        super(Config.class);
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+
+            log.info("Authenticating request :{}",exchange.getRequest().getURI());
+            final String tokenHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+            if (tokenHeader == null || !tokenHeader.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            final String token = tokenHeader.substring(7).trim();
+
+
+            try{
+                String userId = String.valueOf(jwtService.getUserIdFromToken(token));
+                ServerWebExchange mutatedExchange = exchange.mutate()
+                        .request(r -> r.header("X-User-Id",userId))
+                        .build();
+                return chain.filter(mutatedExchange);
+
+            }catch (JwtException e){
+                log.error("jwt exception",e.getLocalizedMessage());
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+        };
+    }
+
+    static class Config {
+
+    }
 }
